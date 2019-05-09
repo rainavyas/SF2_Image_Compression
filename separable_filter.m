13cb68637c5a02f36db201d4f7784cf5b4d60a75
rainavyas <sayvaniar@gmail.com>
2019-05-09 16:56:37 +0100

    compare row-col, col-row filtering

--- separable_filter.m
+++ separable_filter.m
@@ -1,8 +1,20 @@
-function separable_filter(X)
+function val = separable_filter(X)
 
 h = halfcos(15);
 
 %row then column filtering
+R1 = conv2se(1,h,X);
+R2 = conv2se(1,h,transpose(R1));
+R = transpose(R2);
+draw(R);
 
 
 %column then row filtering
+C1 = conv2se(1,h,transpose(X));
+C = conv2se(1,h,transpose(C1));
+draw(C);
+
+%find difference between images
+diff = abs(R-C);
+val = sum(sum(diff));
+return
