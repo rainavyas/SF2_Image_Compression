7247acd05055111e59ef26ded23b4648b5340715
rainavyas <sayvaniar@gmail.com>
2019-05-14 13:34:36 +0100

    add interpolations and created 4-layer pyramid file

---
+++ highpass_from_decimated_lowpass.m
@@ -0,0 +1,18 @@
+function Y0 = highpass_from_decimated_lowpass(X)
+
+h = [0.25, 0.5, 0.25];
+X1 = quarter_size_lowpass(X);
+
+X1_row = rowint(X1, h);
+X1_full = transpose(rowint(transpose(X1_row), h));
+
+Y0 = X - X1_full;
+
+figure(1);
+draw(X1);
+
+figure(2);
+draw(Y0);
+
+
+return
---
+++ py4enc.m
@@ -0,0 +1,4 @@
+function py4enc(X)
+
+%Generate a 4-layer pyramid, so that X is split into Y0, Y1, Y2, Y3 and X4
+
