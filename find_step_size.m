d0d8d0c5486edf9d3e634b4ad3708426edd444b4
rainavyas <sayvaniar@gmail.com>
2019-05-15 10:04:23 +0100

    created find_step_size file

---
+++ find_step_size.m
@@ -0,0 +1,8 @@
+function [step0, step1, step2, step3, step4] = find_step_size(X, step)
+
+% The image X is quantised by step and the rms error is found. stepn is the
+% required step size to quantise a n-layer pyramid that finds the most
+% similar rms error
+
+
+
